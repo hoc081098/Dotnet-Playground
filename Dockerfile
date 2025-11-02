7f3c61de617a1862ce4dfafef9b00cadeecea283
hoc081098@gmail.com <hoc081098@gmail.com>
2025-11-03 01:40:40 +0700

    Add initial project structure and configuration files for C# Playground

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:9.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["CsharpPlayground.csproj", "./"]
+RUN dotnet restore "CsharpPlayground.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "./CsharpPlayground.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./CsharpPlayground.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "CsharpPlayground.dll"]
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CsharpPlayground.OOP;
+
+Console.WriteLine("Hello, World!");
+
+Person hoc = new("Hoc Thai")
+{
+    LastName = "Nguyen",
+    Address = "Da Nang"
+};
+Console.WriteLine($"hoc is {hoc}");
+
+var lastNameLength = hoc.LastName.Length;
+Console.WriteLine($"length of hoc's last name is {lastNameLength}");
+
+hoc.Age = 30;
+Console.WriteLine($"hoc's age is {hoc.Age}");
+
+var addressLength = hoc.Address?.Length;
+Console.WriteLine($"length of hoc's address is {addressLength}");
+
+Person hocClone = new("Hoc Thai")
+{
+    LastName = "Nguyen",
+    Address = "Da Nang",
+    Age = 30
+};
+Console.WriteLine($"hocClone is {hocClone}");
+Console.WriteLine($"hoc equals hocClone: {hoc.Equals(hocClone)}");
+Console.WriteLine($"hoc equals \"string\": {hoc.Equals("string")}");
+Console.WriteLine($"hoc == hocClone: {hoc == hocClone}");
+Console.WriteLine($"hoc's hashcode is {hoc.GetHashCode()}");
+Console.WriteLine($"hocClone's hashcode is {hocClone.GetHashCode()}");
+
+Person.IncrementCount();
+Person.IncrementCount();
+Person.IncrementCount();
+
+// Static readonly fields are initialized only once and are not inlined.
+var n1 = Person.Names;
+var n2 = Person.Names;
+Console.WriteLine($"Are n1 and n2 the same instance? {ReferenceEquals(n1, n2)}");
+
+// Const fields will be inlined at compile time
+var c1 = Person.BoolConst;
+var c2 = Person.BoolConst;
+var c3 = c1 && c2;
+Console.WriteLine($"c1: {c1}, c2: {c2}, c3: {c3}");
