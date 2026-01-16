b2a64aa6c66b1ebd9375a49a0d4aa6f4bc59e77a
hoc081098@gmail.com <hoc081098@gmail.com>
2026-01-16 13:33:16 +0700

    Add F# project and initial console application setup

---
+++ FsharpPlayground/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+[<EntryPoint>]
+let main args =
+    printfn "Hello from F#"
+    printfn "Arguments passed to function : %A" args
+    printfn $"Arguments passed to function : %A{args}"
+    // Return 0. This indicates success.
+    0
