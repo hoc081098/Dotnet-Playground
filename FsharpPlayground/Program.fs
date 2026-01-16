408ea9f01f94588d87c0b6c1ce85a88d93aa7061
hoc081098@gmail.com <hoc081098@gmail.com>
2026-01-16 14:10:23 +0700

    Add UserStruct and UserClass definitions with example usage in Program.fs

--- FsharpPlayground/Program.fs
+++ FsharpPlayground/Program.fs
@@ -11,6 +11,11 @@ let lenOfString (s: string) = s.Length
 let rec fib n =
     if n <= 1 then n else fib (n - 1) + fib (n - 2)
 
+[<Struct>]
+type UserStruct = { name: string; age: int }
+
+type UserClass = { name: string; age: int }
+
 [<EntryPoint>]
 let main args =
     printfn "Hello from F#"
@@ -27,6 +32,11 @@ let main args =
     let idOfStr: string = id "hello"
     let idOfTuple: (int * int * int) list = id [ 1, 2, 3 ]
     let idOfList: int list = id [ 1; 2; 3 ]
+
+    let user: UserStruct = { name = "Hoc"; age = 28 }
+    let copiedUser: UserStruct = { user with age = 30 }
+    printfn $"user is {user}"
+    printfn $"copiedUser is {copiedUser}"
 
     // Return 0. This indicates success.
     0
