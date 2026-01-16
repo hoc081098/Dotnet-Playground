22243376f7c63a1d7f1d89c3f8d1ba5c728216d0
hoc081098@gmail.com <hoc081098@gmail.com>
2026-01-16 13:44:30 +0700

    Add Fibonacci function and sum functions to Program.fs

--- FsharpPlayground/Program.fs
+++ FsharpPlayground/Program.fs
@@ -1,9 +1,22 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
+let sum1 (a: int) (b: int) : int = a + b
+let sum2 a b = a + b
+
+let rec fib n =
+    if n <= 1 then n else fib (n - 1) + fib (n - 2)
+
 [<EntryPoint>]
 let main args =
     printfn "Hello from F#"
     printfn "Arguments passed to function : %A" args
     printfn $"Arguments passed to function : %A{args}"
+
+    let fibOf10 = fib 10
+    printfn $"Fib(10) is {fibOf10}"
+
+    let sumOf2And3 = sum1 2 3
+    let sumOf3And4 = sum2 3 4
+
     // Return 0. This indicates success.
     0
